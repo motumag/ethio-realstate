d7213e88eb275db4041e75b959c9832b0b1bcb17
motumag <motumagishu27@gmail.com>
2023-10-11 16:19:09 +0300

    Complete sign up page ui design

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,37 @@
+import { Link } from "react-router-dom";
 export default function SignUp() {
-  return <div>Sign Up</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold">Sign Up</h1>
+      <form className="flex flex-col gap-4 my-7">
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg focus:outline-none"
+          id="email"
+        />
+        <input
+          type="Password"
+          placeholder="password"
+          className="border p-3 rounded-lg focus:outline-none"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white p-3 hover:opacity-90 rounded-lg uppercase disabled:opacity-80">
+          sign up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- server/utils/error.js
+++ server/utils/error.js
@@ -1,4 +1,4 @@
-const errorHandler = (statusCode, message) => {
+export const errorHandler = (statusCode, message) => {
   const error = new Error();
   (error.statusCode = statusCode), (error.message = message);
   return error;
