e5859641f9b5015c8bade25a5adae004efa73cf8
motumag <motumagishu27@gmail.com>
2023-10-11 09:04:27 +0300

    header, link and navigation completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import SignUp from "./pages/SignUp";
 import SingIn from "./pages/SingIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SingIn />} />
