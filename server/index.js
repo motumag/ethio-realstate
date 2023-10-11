208ac0db7bc291d22b305febc2eeb4f16aa86914
motumag <motumagishu27@gmail.com>
2023-10-11 10:09:10 +0300

    mongodb setup completed with .env

--- server/index.js
+++ server/index.js
@@ -1,4 +1,15 @@
 import exppress from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Connected to mongodb successfully");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 const app = exppress();
 
 app.listen(3000, () => {
