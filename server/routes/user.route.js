48be7a45fe5a09369a0beaf7f8022d76c9fbf28d
motumag <motumagishu27@gmail.com>
2023-10-16 18:33:02 +0300

    Delete user route has done

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { updateUserInfo } from "../controllers/user.controller.js";
+import { updateUserInfo, deleteUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/userVerification.js";
 const router = express.Router();
 router.post("/update/:id", verifyToken, updateUserInfo);
+router.delete("/delete/:id", verifyToken, deleteUser);
 export default router;
